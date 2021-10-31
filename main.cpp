45a770d7a6d31a2f1f02a62f9ed588b7cb1d9d65
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2021-10-31 18:03:06 +0000

    Basic C++ Project

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "stdio.h"
+using namespace std;
+
+/*
+https://code.visualstudio.com/docs/languages/cpp
+
+https://code.visualstudio.com/docs/cpp/config-mingw
+
+This is using https://www.msys2.org/
+*/
+
+int main()
+{
+    printf("");
+    cout << "ELEC350" << endl;
+    cout << "ELEC351" << endl;
+    
+    return 0;
+
+}
